4311c0c3fcab4b9d94926855f3e729577ba27a5c
juleslepitschka <slepitschkaju84293>
2022-08-16 06:55:31 +0200

    update-content-correct

--- weather-project/src/components/Content/CurrentContent/CurrentContent.js
+++ weather-project/src/components/Content/CurrentContent/CurrentContent.js
@@ -5,14 +5,15 @@ import { useSelector } from "react-redux";
 const CurrentContent = () => {
   const cityName = useSelector((state) => state.current.cityName);
   const temp = useSelector((state) => state.current.temp);
-  const maxTemp = useSelector((state) => state.current.maxtemp);
-  const minTemp = useSelector((state) => state.current.mintemp);
+  const maxTemp = useSelector((state) => state.current.maxTemp);
+  const minTemp = useSelector((state) => state.current.minTemp);
 
   return (
     <Card className={classes.content}>
       <div>Das heutige Wetter</div>
       <div>Stadt: {cityName} </div>
-      <div>Aktuelle Temperatur: {temp} Kelvin Maximale Temperatur: {maxTemp} Kelvin </div>
+      <div>Aktuelle Temperatur: {temp} Kelvin</div>
+      <div>Kelvin Maximale Temperatur: {maxTemp} Kelvin </div>
       <div>Minimale Temperatur: {minTemp} Kelvin </div>
     </Card>
   );
