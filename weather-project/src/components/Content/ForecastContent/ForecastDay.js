5270c65b776a487b9acebaa347f61e0a58926d7b
juleslepitschka <slepitschkaju84293>
2022-08-18 12:52:04 +0200

    forecast-date-shorter

--- weather-project/src/components/Content/ForecastContent/ForecastDay.js
+++ weather-project/src/components/Content/ForecastContent/ForecastDay.js
@@ -1,37 +1,24 @@
 import classes from "./ForecastDay.module.css";
-// import { useState } from "react";
 import Card from "../../UI/Card";
 import useDateConverter from "../../../hooks/use-dateConverter";
 import useTimestampConverter from "../../../hooks/use-timestampConverter";
 
 const ForecastDay = (props) => {
-  // const [equalDate, setEqualDate] = useState(false);
   const { timestamp, dateString, temp } = props;
 
-  const x = useTimestampConverter(timestamp);
+  const today = useDateConverter(new Date()); //array
 
-  const today = useDateConverter(new Date());
-  const dateArray = useDateConverter(x);
+  const dateTS = useTimestampConverter(timestamp);
+  const dateArray = useDateConverter(dateTS); //array
+
+  const equalDate = JSON.stringify(today) === JSON.stringify(dateArray);
+
   const { day, dayDate, monthDate } = dateArray;
-
-  // if (
-  //   today.day === day &&
-  //   today.dayDate === dayDate &&
-  //   today.monthDate === monthDate
-  // ) {
-  //   setEqualDate(true);
-  // }
-
+  
   return (
     <li className={classes.element}>
       <Card>
-        {today.day === day &&
-        today.dayDate === dayDate &&
-        today.monthDate === monthDate ? (
-          <h1>Heute</h1>
-        ) : (
-          <h1>{dayDate}</h1>
-        )}
+        {equalDate ? <h1>Heute</h1> : <h1>{dayDate}</h1>}
         <p>
           {day}. {monthDate}
         </p>
